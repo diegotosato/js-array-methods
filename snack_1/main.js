873cff98b1ed06dd4364f54aed3576e84ac588d0
diegotosato <tosato1998@gmail.com>
2025-10-22 12:18:33 +0200

    import array of cars

--- snack_1/main.js
+++ snack_1/main.js
@@ -10,3 +10,15 @@ nel terzo il resto delle auto.
 Infine stampa separatamente i 3 array.
 */
 
+const cars = [
+  { brand: 'Ford', model: 'Fiesta', fuel: 'benzina' },
+  { brand: 'Toyota', model: 'Corolla', fuel: 'diesel' },
+  { brand: 'Honda', model: 'Civic', fuel: 'gpl' },
+  { brand: 'Tesla', model: 'Model 3', fuel: 'elettrico' },
+  { brand: 'Fiat', model: 'Panda', fuel: 'metano' },
+  { brand: 'Volkswagen', model: 'Golf', fuel: 'benzina' },
+  { brand: 'BMW', model: 'Serie 3', fuel: 'diesel' },
+  { brand: 'Audi', model: 'A4', fuel: 'gpl' },
+  { brand: 'Mercedes', model: 'Classe C', fuel: 'elettrico' },
+  { brand: 'Peugeot', model: '208', fuel: 'metano' }
+]
