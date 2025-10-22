639689765af65374b63b670fdad915f1b26c367f
diegotosato <tosato1998@gmail.com>
2025-10-22 12:27:18 +0200

    create 3 new arrays with filter array method

--- snack_1/main.js
+++ snack_1/main.js
@@ -11,38 +11,70 @@ Infine stampa separatamente i 3 array.
 */
 
 const cars = [
-  { brand: 'Ford', model: 'Fiesta', fuel: 'benzina' },
-  { brand: 'Toyota', model: 'Corolla', fuel: 'diesel' },
-  { brand: 'Honda', model: 'Civic', fuel: 'gpl' },
-  { brand: 'Tesla', model: 'Model 3', fuel: 'elettrico' },
-  { brand: 'Fiat', model: 'Panda', fuel: 'metano' },
-  { brand: 'Volkswagen', model: 'Golf', fuel: 'benzina' },
-  { brand: 'BMW', model: 'Serie 3', fuel: 'diesel' },
-  { brand: 'Audi', model: 'A4', fuel: 'gpl' },
-  { brand: 'Mercedes', model: 'Classe C', fuel: 'elettrico' },
-  { brand: 'Peugeot', model: '208', fuel: 'metano' }
+    { brand: 'Ford', model: 'Fiesta', fuel: 'benzina' },
+    { brand: 'Toyota', model: 'Corolla', fuel: 'diesel' },
+    { brand: 'Honda', model: 'Civic', fuel: 'gpl' },
+    { brand: 'Tesla', model: 'Model 3', fuel: 'elettrico' },
+    { brand: 'Fiat', model: 'Panda', fuel: 'metano' },
+    { brand: 'Volkswagen', model: 'Golf', fuel: 'benzina' },
+    { brand: 'BMW', model: 'Serie 3', fuel: 'diesel' },
+    { brand: 'Audi', model: 'A4', fuel: 'gpl' },
+    { brand: 'Mercedes', model: 'Classe C', fuel: 'elettrico' },
+    { brand: 'Peugeot', model: '208', fuel: 'metano' }
 ]
 
 
-const fuelCars = []
-const dieselCars = []
-const otherCars = []
+// const fuelCars = []
+// const dieselCars = []
+// const otherCars = []
 
 
 
 
 // With For loop
-for (let i = 0; i < cars.length; i++) {
-    const thisCar = cars[i];
-    if(thisCar.fuel === 'benzina'){
-        fuelCars.push(thisCar)
-    } else if (thisCar.fuel === 'diesel'){
-        dieselCars.push(thisCar)
-    } else {
-        otherCars.push(thisCar)
-    }
-}
+// for (let i = 0; i < cars.length; i++) {
+//     const thisCar = cars[i];
+//     if(thisCar.fuel === 'benzina'){
+//         fuelCars.push(thisCar)
+//     } else if (thisCar.fuel === 'diesel'){
+//         dieselCars.push(thisCar)
+//     } else {
+//         otherCars.push(thisCar)
+//     }
+// }
 
+// console.log(fuelCars);
+// console.log(dieselCars);
+// console.log(otherCars);
+
+
+
+//With arrays methods
+
+const fuelCars = cars.filter((thisCar) => {
+    if (thisCar.fuel === 'benzina') {
+        return true
+    }
+    return false
+})
 console.log(fuelCars);
+
+
+
+const dieselCars = cars.filter((thisCar) => {
+    if (thisCar.fuel === 'diesel') {
+        return true
+    }
+    return false
+})
 console.log(dieselCars);
+
+
+
+const otherCars = cars.filter((thisCar) => {
+    if (thisCar.fuel !== 'benzina' && thisCar.fuel !== 'diesel') {
+        return true
+    }
+    return false
+})
 console.log(otherCars);
